9867d605f1761dad2a5ed92282e3357e4be53eed
Wallace Viana <wall.47.viana@gmail.com>
2022-02-16 23:11:59 -0300

    feat(web): initial web setup

---
+++ frontend/web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import GlobalStyle from './styles/global'
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello world</h1>
+      </div>
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
